f65b5404f4be0a25314a7da05e5f5fcab1449387
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-09-06 12:18:35 -0300

    micro paso 6

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express');
+const express = require("express");
+const resolve = require("path");
+const path = require("path");
 const app = express();
+const mainRoutes = require("./routers/main")
 
-app.listen (3000, () => {
-    console.log('servidor funcionando');
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(mainRoutes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor corriendo, puerto 3000");
 });
+app.get("/",);
+app.get("/",);
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,1 +1,12 @@
-const mainController = {};
+const path = require("path")
+
+const mainController = {
+    home:  (req, res) => {
+        res.sendFile(path.join(__dirname, "../views/home.html"));
+    },
+    about:  (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../views/about.html"));
+      },
+};
+
+module.exports = mainController;
